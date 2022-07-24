7bb372ca5689338cc7f740d984f16bcb367a39a1
jaallaall <jalal.khaleqi22@gmail.com>
2022-07-25 00:01:35 +0430

    feat:set lang rtl

--- src/components/Header/Lang.tsx
+++ src/components/Header/Lang.tsx
@@ -1,38 +1,30 @@
-import i18next from "i18next";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 const Lang: React.FC = (): React.ReactElement => {
-  const [show, setShow] = useState<{ en: boolean; fa: boolean }>({
-    en: true,
-    fa: false,
-  });
-  const handleChangeLangEn = () => {
-    i18next.changeLanguage("en");
-    setShow({ fa: true, en: false });
-  };
-  const handleChangeLangFa = () => {
-    i18next.changeLanguage("fa");
-    setShow({ fa: false, en: true });
-  };
+  const { i18n } = useTranslation();
+  const [lang, setLang] = useState<string>("en");
+
+  function handleChangeTheme(e: React.ChangeEvent<HTMLInputElement>) {
+    i18n.changeLanguage(e.target.checked ? "fa" : "en");
+    document.dir = i18n.dir();
+    setLang(e.target.checked ? "en" : "fa");
+  }
+
   return (
-    <div className="flex ms-2">
-      {show.en && (
-        <button
-          className="w-10 h-10 border border-gray-200 rounded-md flex items-center justify-center"
-          onClick={handleChangeLangEn}
-        >
-          <img src="/static/images/en.png" className="w-5 h-5" />
-        </button>
+    <label className="cursor-pointer flex items-center justify-center border border-gray-200 rounded-md w-10 h-10 ms-2 hover:bg-primary hover:text-white hover:border-primary text-gray-400">
+      <input
+        type="checkbox"
+        checked={lang === "en"}
+        onChange={handleChangeTheme}
+        hidden
+      />
+      {lang === "en" ? (
+        <img src="/static/images/en.png" className="w-5 h-5" />
+      ) : (
+        <img src="/static/images/ir.png" className="w-5 h-5" />
       )}
-      {show.fa && (
-        <button
-          className="w-10 h-10 border border-gray-200 rounded-md flex items-center justify-center"
-          onClick={handleChangeLangFa}
-        >
-          <img src="/static/images/ir.png" className="w-5 h-5" />
-        </button>
-      )}
-    </div>
+    </label>
   );
 };
 
